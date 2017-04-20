a0b8945d75a716290ad9dd165c51049435b3ce94
Eugeny Dementev <eugeny.dementev@ya.ru>
2017-04-20 13:12:34 +0800

    Update build

--- dist/middleware.js
+++ dist/middleware.js
@@ -12,9 +12,9 @@ module.exports = function configureMiddleware() {
       logSuccess = _params$logSuccess === undefined ? function () {} : _params$logSuccess;
 
 
-  return function middleware(store) {
+  return function appstateMiddleware(store) {
     return function (next) {
-      return function actionApplier(actions, args) {
+      return function signalExecutor(actions, args) {
         if (!Array.isArray(actions)) {
           for (var _len = arguments.length, rest = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
             rest[_key - 2] = arguments[_key];
